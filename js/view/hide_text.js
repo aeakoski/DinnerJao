356fcd9a995958b35dc00159d9e593f4002f2120
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-11 16:34:21 +0100

    number of people added with right menu div

--- js/view/hide_text.js
+++ js/view/hide_text.js
@@ -5,8 +5,24 @@ $(document).ready(function() {
     $("#startButton").click(function () {
        	//code for what hapens when the startButton is clicked
        	$("#welcome").fadeOut();
+       	$("body").css("background-image", "none");
     });
 
+    function getNumOfPeople () {
+    	// body...
+    	var numOfPeople = $('#num').val();
+    	//hide the num of people selected
+    	$("#showtest").html(numOfPeople).hide();
+    }
+
+
+    //Reads value of selector num of people
+    $("select").change( getNumOfPeople );
+    getNumOfPeople();
+
+    
+
+    
 
 
 
