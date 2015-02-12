c21495256d9ffe771430840256cc5b491efff1ed
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-12 10:17:20 +0100

    added some javascript to the model

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -3,20 +3,24 @@ var DinnerModel = function() {
  
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
-
+	var attendies;
+	var typeOfDish;
 
 	this.setNumberOfGuests = function(num) {
 		//TODO Lab 2
-	}
+    	attendies = num;						//Kanske funkar
+    }
 
 	// should return 
 	this.getNumberOfGuests = function() {
 		//TODO Lab 2
+		return attendies;
 	}
 
 	//Returns the dish that is on the menu for selected type 
 	this.getSelectedDish = function(type) {
 		//TODO Lab 2
+
 	}
 
 	//Returns all the dishes on the menu.
--- js/view/hide_text.js
+++ js/view/hide_text.js
@@ -29,8 +29,4 @@ $(document).ready(function() {
 
     
 
-    
-
-
-
 });
