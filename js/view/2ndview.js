f17ac7e729a383a226d00171b2a6a3b1d802cbfd
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-12 19:39:49 +0100

    all alone on clek night

--- js/view/2ndview.js
+++ js/view/2ndview.js
@@ -6,28 +6,33 @@ var SndView = function (container,model) {
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.dishList = container.find("#dishList");
 	this.totalCost = container.find("#totalCost");
+	this.mealType = container.find("#mealType");
 	
 	this.totalCost.html(model.getTotalMenuPrice());
 
 	this.numberOfGuests.html(model.getNumberOfGuests());
 
-	var starters = model.getAllDishes("starter");
 
-	for (i = 0; i<starters.length; i++) {
+
+	console.log(this.mealType.change());
+
+	var typeOfDish = model.getAllDishes(this.mealType.val());
+
+	for (i = 0; i<typeOfDish.length; i++) {
 		console.log("i for-loopen");
 		this.dishList.append('\
 			<div class="foodItem">\
 				<div class="foodHead">\
 					<div class = "foodPic">\
-						<img src="images/'+ starters[i]['image'] +'">\
+						<img src="images/'+ typeOfDish[i]['image'] +'">\
 					</div>\
 					<div class = "foodTitle">\
-						<h4>'+ starters[i]['name'] +'</h4>\
+						<h4>'+ typeOfDish[i]['name'] +'</h4>\
 					</div>\
 				</div>\
 				\
 				<div class = "foodDesc">\
-					<p>'+ starters[i]['description'] +'</p>\
+					<p>'+ typeOfDish[i]['description'] +'</p>\
 				</div>\
 			</div><!--FoodItem-->');
 
