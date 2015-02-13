8c1ab2ae27f1463971276e6a243f409f7a80c230
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-13 12:51:54 +0100

    fixed some css

--- js/view/3rdview.js
+++ js/view/3rdview.js
@@ -60,29 +60,4 @@ var SndView = function (container,model) {
 
 
 
-	var typeOfDish = model.getAllDishes(this.mealType.val());
-
-	for (i = 0; i<typeOfDish.length; i++) {
-		console.log("i for-loopen");
-		this.dishList.append('\
-			<div class="foodItem">\
-				<div class="foodHead">\
-					<div class = "foodPic">\
-						<img src="images/'+ typeOfDish[i]['image'] +'">\
-					</div>\
-					<div class = "foodTitle">\
-						<h4>'+ typeOfDish[i]['name'] +'</h4>\
-					</div>\
-				</div>\
-				\
-				<div class = "foodDesc">\
-					<p>'+ typeOfDish[i]['description'] +'</p>\
-				</div>\
-			</div><!--FoodItem-->');
-
-	}
-
-	console.log("i SndView.js filen");
-
-
 }
