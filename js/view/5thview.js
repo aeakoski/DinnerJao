ef7838d1c2f8f80ac77011e22eba04d3d9c3ef30
aeakoski <aeakoski@yahoo.se>
2015-02-13 16:18:59 +0100

    sdfghgfd

--- js/view/5thview.js
+++ js/view/5thview.js
@@ -9,23 +9,24 @@ var FifthView = function (container,model) {
 	var dish = model.getFullMenu();
 
 
-	this.PrintMenuItem = container.find("#printFoodItem");
+	this.printMenuItem = container.find("#printFoodItem");
 
 
-	for (var g = 0; g<dish.length; g++) {
-
+	for (var h = 0; h<dish.length; h++) {
+		console.log("skriver ut!!!!!!");
+		this.printMenuItem("sdfghjkjhgfdsdfghjk")
 		this.printMenuItem.append('\
 					<div class="col-xs-3">\
-					<img src="image/'+ dish[g]['imgage'] +'">\
+					<img src="image/'+ dish[h]['imgage'] +'">\
 					</div>\
 					<div class="col-xs-4">\
-						<h2>'+ dish[g]['name'] + '</h2>\
+						<h2>'+ dish[h]['name'] + '</h2>\
 						<p>asdfghjasfghjsdfghjk</p>\
 					</div>\
 					<div class="col-xs-5">\
 						<h3>preparation</h3>\
 					</div>');
-		console.log(i);
+		console.log(h);
 	
 	}
 
