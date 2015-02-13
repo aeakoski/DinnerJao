745a4830e6d35664de916bc4c55b58d19feefdd7
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-13 14:23:08 +0100

    fixed some java for the last view

--- js/view/4thview.js
+++ js/view/4thview.js
@@ -11,20 +11,14 @@ var FourthView = function (container,model) {
 	this.menuItem = container.find("#menuItem");
 	for (i = 0; i<dish.length; i++) {
 		this.menuItem.append('\
-				<div class="foodItem">\
-					<div class="foodHead">\
+					<div class="foodHead col-xs-2" >\
 						<div class = "foodPic">\
 							<img src="images/'+ dish[i]['image'] +'">\
 						</div>\
 						<div class = "foodTitle">\
 							<h4>'+ dish[i]['name'] +'</h4>\
 						</div>\
-					</div>\
-					\
-					<div class = "foodDesc">\
-						<p>'+ dish[i]['description'] +'</p>\
-					</div>\
-				</div><!--FoodItem-->');
+					</div>');
 	}
 
 }
