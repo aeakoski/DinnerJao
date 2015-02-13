5626a32e457b63df7bbd9fdae9229c09db91838c
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-13 16:12:24 +0100

    aksjdha

--- js/view/5thview.js
+++ js/view/5thview.js
@@ -1,26 +1,23 @@
 var FifthView = function (container,model) {
 
-	$("body").css("backgroung-image","none");
+	$("body").css("background-image","none");
 
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.numberOfGuests.html(model.getNumberOfGuests());
 	//this.totalCost = container.find("#totalCost");
-
-	this.totalCost.html(model.getTotalMenuPrice());
 	
 	var dish = model.getFullMenu();
 
-	this.PrintMenuItem = container.find("#fifthView");
+	this.printMenuItem = container.find("#printFoodItem");
 
 	for (var g = 0; g<dish.length; g++) {
 
 		this.printMenuItem.append('\
-
-					<div class="col-xs-3">
+					<div class="col-xs-3">\
 					<img src="image/'+ dish[g]['imgage'] +'">\
 					</div>\
 					<div class="col-xs-4">\
-						<h2>'+ dish[g]['name']  +'</h2>\
+						<h2>'+ dish[g]['name'] + '</h2>\
 						<p>asdfghjasfghjsdfghjk</p>\
 					</div>\
 					<div class="col-xs-5">\
