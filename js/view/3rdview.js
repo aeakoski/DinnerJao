948ca1185ccfae2dad8309492ee6a6bb319f8e38
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-17 15:03:43 +0100

    lsajd

--- js/view/3rdview.js
+++ js/view/3rdview.js
@@ -2,37 +2,25 @@ var ThreeView = function (container,model) {
 
 	$("body").css("backgroung-image","none");
 
-	
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.dishList = container.find("#dishList");
 	this.totalCost = container.find("#totalCost");
 	this.mealType = container.find("#mealType");
 	this.prepDish = container.find("#prepDish");
 	this.ingredientsTable = container.find("#ingredients");
-	//this.dishCost = container.find("#dishCost");
-
 	this.dPending = container.find("#dPending");
-
 
 	
 	this.totalCost.html(model.getTotalMenuPrice());
-
 	this.numberOfGuests.html(model.getNumberOfGuests());
 
-	//var totalCostForDish = model.getDishCost(model.getDish(1)); //Här ska dden valda rätten fyllas i!
-
-	//this.dishCost.append(totalCostForDish);
-
+	
 	var menuList = model.getFullMenu();
-
 	for (var i = 0; i < menuList.length; i++) {
-
-
-
 		this.dPending.append('\
 			<p class="dName"><span>'+menuList[i]+'</span></p>\
 			<p class="dCost"><span>0.00</span></p>');
-	 };
+	}
 
 
 	var selectedDish = model.getSelectedDish("starter");
@@ -42,11 +30,7 @@ var ThreeView = function (container,model) {
 		<p>' + selectedDish['description'] + '</p>');
 
 
-
-
 	var ingredients = model.getAllIngredients();
-
-
 	for (var i = 0; i < ingredients.length; i++) {
 		this.ingredientsTable.append('\
 			<tr>\
@@ -55,9 +39,5 @@ var ThreeView = function (container,model) {
 				<td>SEK</td>\
 				<td>'+ingredients[i]["price"]+'</td>\
 			</tr>');
-		
-	};
-
-
-
+	}
 }
