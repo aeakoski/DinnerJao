1d7f02dcface0474c97360d60f4f5e46832a9ea0
Jonas.J <jan.jonas.johansson@gmail.com>
2015-02-17 16:07:08 +0100

    asd

--- js/view/5thview.js
+++ js/view/5thview.js
@@ -14,15 +14,15 @@ var FifthView = function (container,model) {
 		this.printMenuItem.append('\
 			<div id = "upperDish" class = col-xs-12>\
 				<div class="col-xs-3">\
-				<img src="images/'+ dish[h]['image'] +'">\
+				<img src="images/'+ dish[i]['image'] +'">\
 				</div>\
 				<div class="col-xs-4">\
-					<h2>'+ dish[h]['name'] + '</h2>\
+					<h2>'+ dish[i]['name'] + '</h2>\
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales tempus pellentesque. Vestibulum convallis ullamcorper sollicitudin. Integer ut faucibus sapien, eget feugiat risus. Aliquam nisi erat, posuere nec euismod eu, fermentum nec nisl. Aenean id ornare nunc. Nunc aliquam nisl magna. Proin id hendrerit urna..</p>\
 				</div>\
 				<div class="col-xs-5">\
 					<h3>Preparation</h3>\
-					<p>'+ dish[h]['description'] +'</p>\
+					<p>'+ dish[i]['description'] +'</p>\
 				</div>\
 			</div>');
 	}
